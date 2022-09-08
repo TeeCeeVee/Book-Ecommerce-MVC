9ef5833b8c7680247b4fb83066887b970f9b4978
thea Villanueva <tei.villanueva@gmail.com>
2022-09-08 13:43:28 -0700

    Add Category Controller

--- EcommerceBookWeb/Controllers/CategoryController.cs
+++ EcommerceBookWeb/Controllers/CategoryController.cs
@@ -1,11 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EcommerceBookWeb.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EcommerceBookWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        //connects the controller to database
+        //member variable
+        //AppDbContext is a data type
+        private readonly AppDbContext _db;
+
+        public CategoryController(AppDbContext db)
+        {
+            //use _db to retrieve categories
+            _db = db;
+        }
+
         public IActionResult Index()
         {
+            //convert the categories to a list and assign to objCategoryList
+            var objCategoryList = _db.Categories.ToList();
             return View();
         }
     }
