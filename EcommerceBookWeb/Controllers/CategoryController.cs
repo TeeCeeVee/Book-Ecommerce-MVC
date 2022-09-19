8227b1928348f869d42273fc2031d11696fa5775
thea Villanueva <tei.villanueva@gmail.com>
2022-09-19 08:42:48 -0700

    Create Edit route and edit page

--- EcommerceBookWeb/Controllers/CategoryController.cs
+++ EcommerceBookWeb/Controllers/CategoryController.cs
@@ -23,13 +23,13 @@ namespace EcommerceBookWeb.Controllers
             return View(objCategoryList);
         }
 
-        //GET
+        //CREATE GET
         public IActionResult Create()
         {
             return View();
         }
 
-        //POST
+        // CREATE POST
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Create(Category obj)
@@ -50,6 +50,50 @@ namespace EcommerceBookWeb.Controllers
             return View(obj);
 
         }
+
+        // EDIT GET
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            //three ways to retrieve category based on ID
+            var categoryFromDb = _db.Categories.Find(id);
+            //var categoryFromDbFirst = _db.Categories.FirstOrDefault(u => u.Id == id); // throw exception if one or more elements
+            //var categoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id); //return empty if no elements
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            //otherwise found return on View
+            return View(categoryFromDb);
+        }
+
+        // EDIT POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            //custom validation
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The display order cannot match the Name.");
+            }
+            //create validation
+            if (ModelState.IsValid)
+            {
+                //create record inside database
+                _db.Categories.Add(obj);
+                _db.SaveChanges(); // saves and goes to database
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+
+        }
+
 
 
     }
